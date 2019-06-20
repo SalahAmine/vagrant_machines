7e047b8c582e965d38ec476f3e0a1d0f21cbe2f9
Salah-Amine SELLAMI <amin2sellami@gmail.com>
2019-06-20 16:35:19 +0200

    added other declarations

---
+++ centos7_machine_wordpress/provision_machine.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# sudo pip install --user ansible
+
+# # sudo yum -y update
+# sudo yum -y install  nano
+# sudo yum -y install wget unzip
+# sudo  wget http://wordpress.org/latest.zip
+# ## Install php-gd in order to work with images, install plugins etc…
+# sudo yum -y install php-gd
+# ## If you run yum install mysql on CentOS 7, it is MariaDB that is installed rather than MySQL
+# sudo yum -y install mariadb mariadb-server mysql
